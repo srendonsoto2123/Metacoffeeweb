efdbf57619c070ea0e608ea5ca843d629fc32f1a
srendonsoto2123 <srendonsoto@gmail.com>
2022-05-07 23:35:02 -0500

    Mas cambios

--- src/pages/marketplace.tsx
+++ src/pages/marketplace.tsx
@@ -4,11 +4,6 @@ import { MoralisProvider } from 'react-moralis';
 import Layout from '../components/layout';
 import Market from '../components/market';
 
-const realizarCambios = (e) => {
-   e.preventDefault();
-   console.log("estoy presentando cambios");
-};
-
 const MarketPlace: NextPage = () => {
    return (
       <MoralisProvider appId={ process.env.NEXT_PUBLIC_APPID } serverUrl={ process.env.NEXT_PUBLIC_SERVERURL }>
