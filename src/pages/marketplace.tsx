1a9b13a3e2d053a6fcb398d6bfa29f045b15cad8
srendonsoto2123 <srendonsoto@gmail.com>
2022-05-08 00:00:00 -0500

    Corrigiendo moralis

--- src/pages/marketplace.tsx
+++ src/pages/marketplace.tsx
@@ -6,7 +6,11 @@ import Market from '../components/market';
 
 const MarketPlace: NextPage = () => {
    return (
-      <MoralisProvider appId={ process.env.NEXT_PUBLIC_APPID } serverUrl={ process.env.NEXT_PUBLIC_SERVERURL }>
+      <MoralisProvider 
+         appId={ process.env.NEXT_PUBLIC_APPID }
+         serverUrl={ process.env.NEXT_PUBLIC_SERVERURL }
+         initializeOnMount={false}
+      >
          <Market />
       </MoralisProvider>
    );
